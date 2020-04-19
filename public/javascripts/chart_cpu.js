52966532b68dadf2e35b9b04d6cb4c6d6e30bbbf
menkyukaiden <fateh.ferdjaoui@gmail.com>
2020-04-19 17:34:48 +0200

    Improvment

--- public/javascripts/chart_cpu.js
+++ public/javascripts/chart_cpu.js
@@ -34,7 +34,7 @@ window.addEventListener("load", () => {
                 lineTension: 0.1,
                 borderWidth: 1
             }],
-            labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+            labels: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'],
         },
         options: {
             animation: {
@@ -77,7 +77,7 @@ window.addEventListener("load", () => {
                 lineTension: 0.1,
                 borderWidth: 1
             }],
-            labels: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+            labels: ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0']
         },
         options: {
             animation: {
